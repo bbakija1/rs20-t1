2daec0bc7571d69b0f17333115f7ea61acded323
Benjamin Bakija <benjaminbakija20@gmail.com>
2021-03-17 18:54:07 +0100

    Drugi zadatak

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -2,11 +2,18 @@ package sample;
 
 import javafx.event.ActionEvent;
 import javafx.scene.control.Label;
+import javafx.scene.layout.GridPane;
+
+import java.awt.*;
+
+import static java.awt.Color.*;
 
 public class Controller {
     public Label helloWorld;
 
+
     public void Submit(ActionEvent actionEvent) {
         helloWorld.setText("Hello World!");
+        helloWorld.setStyle("-fx-background-color: pink");
     }
 }
